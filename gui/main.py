7f82751bf4d33c95246d8cbc88a3232f54ee1511
yingjieho <yingjieho@micron.com>
2024-09-15 16:31:30 +0800

    adding reload, grid btn, zoom fn

--- gui/main.py
+++ gui/main.py
@@ -10,7 +10,11 @@ from matplotlib.backends.backend_tkagg import (
     NavigationToolbar2Tk
 )
 from matplotlib.figure import Figure
+import matplotlib.pyplot as plt
 from PIL import Image, ImageTk
+import cv2
+from mpl_interactions import ioff, panhandler, zoom_factory
+import numpy as np
 
 
 SUPPORTED_IMAGE_FORMATS = ('.jpg', '.jpeg', '.png')
@@ -21,7 +25,7 @@ class FilesNav:
         self.files = []
         self.index = -1
 
-    def update(self, top_dir, file=None):
+    def update(self, top_dir, file=None, excl_suffixes=[]):
         if self.files:
             self.files = []
             self.index = -1
@@ -30,9 +34,18 @@ class FilesNav:
             self.files.append(file)
             return
         for file in os.listdir(top_dir):
-            if file.lower().endswith(SUPPORTED_IMAGE_FORMATS):
-                self.files.append(file)
+            file: str
+            if not file.lower().endswith(SUPPORTED_IMAGE_FORMATS):
+                continue
+            if excl_suffixes and file.endswith(excl_suffixes):
+                continue
+            self.files.append(file)
         return
+
+    def current(self):
+        if self.index < 0 or self.index >= len(self.files):
+            return None
+        return (self.top_dir, self.files[self.index])
 
     def next(self):
         if self.index < len(self.files):
@@ -49,7 +62,7 @@ class FilesNav:
 file_nav = FilesNav()
 
 class Toolbar(tk.Frame):
-    def __init__(self, root, main_app):
+    def __init__(self, root, main_app, excl_suffix=None):
         super().__init__(root)
         self.root = root
         self.main_app = main_app
@@ -57,10 +70,13 @@ class Toolbar(tk.Frame):
         self.open_dir_button = tk.Button(self, text="Open Directory", command=self.open_dir)
         self.next_image_button = tk.Button(self, text="Next Image", command=self.next_image)
         self.prev_image_button = tk.Button(self, text="Previous Image", command=self.prev_image)
-        self.open_file_button.grid()
-        self.open_dir_button.grid()
-        self.next_image_button.grid()
-        self.prev_image_button.grid()
+        self.reload_button = tk.Button(self, text="Reload Image", command=self.reload_image)
+        self.open_file_button.grid(row=0, column=0)
+        self.open_dir_button.grid(row=0, column=1)
+        self.next_image_button.grid(row=0, column=2)
+        self.prev_image_button.grid(row=0, column=3)
+        self.reload_button.grid(row=0, column=4)
+        self.excl_suffix = excl_suffix
         self.grid()
         
     def open_file(self):
@@ -71,10 +87,9 @@ class Toolbar(tk.Frame):
 
     def open_dir(self):
         top_dir = filedialog.askdirectory()
-        file_nav.update(top_dir)
+        file_nav.update(top_dir, excl_suffix=self.excl_suffix)
         print("Top dir: ", file_nav.top_dir)
         print("Files: ", file_nav.files)
-
 
     def next_image(self):
         rv = file_nav.next()
@@ -90,6 +105,11 @@ class Toolbar(tk.Frame):
         else:
             print("No more images in directory")
 
+    def reload_image(self):
+        rv = file_nav.current()
+        if rv is not None:
+            self.main_app.process_reload_image(*rv)
+
 def on_click(event):
     """
     to support
@@ -98,25 +118,6 @@ def on_click(event):
     - [ ] circle (3 clicks)
     """
     print(event.xdata, event.ydata)
-
-class ResizingCanvas(tk.Canvas):
-    def __init__(self,parent,**kwargs):
-        super().__init__(parent,**kwargs)
-        self.bind("<Configure>", self.on_resize)
-        self.height = self.winfo_reqheight()
-        self.width = self.winfo_reqwidth()
-
-    def on_resize(self,event):
-        # determine the ratio of old width/height to new width/height
-        wscale = float(event.width)/self.width
-        hscale = float(event.height)/self.height
-        self.width = event.width
-        self.height = event.height
-        # resize the canvas 
-        self.config(width=self.width, height=self.height)
-        # rescale all the objects tagged with the "all" tag
-        self.scale("all",0,0,wscale,hscale)
-
 
 class MainApplication:
     def __init__(self, *args, **kwargs):
@@ -134,30 +135,43 @@ class MainApplication:
         self.toolbar = Toolbar(self.root, self)
         # self.canvas = None
         self.canvas = tk.Canvas(self.root, bg='red')
-        print(self.canvas.winfo_reqwidth(), self.canvas.winfo_reqheight())
         # self.canvas = ResizingCanvas(self.root)
         self.canvas.grid(row=1, column=0, sticky='nsew')
-    
-    def create_widgets(self):
-        self.quit_button = tk.Button(self, text="Quit", command=self.quit)
-        self.quit_button.grid()
 
-    def _process_image(self, top_dir, fname):
+    def _process_image(self, top_dir, fname, xdata=None, ydata=None):
+        self.top_dir = top_dir
+        self.fname = fname
         self.canvas.destroy()
         self.canvas = tk.Canvas(self.root)
         self.canvas.grid()
-        im = Image.open(os.path.join(top_dir, fname))
-        image = ImageTk.PhotoImage(im)
-        fig = Figure(dpi=200)  # make option
-        fig.add_subplot(111).imshow(im)
+        im = cv2.imread(os.path.join(top_dir, fname))
+        im = cv2.cvtColor(im, cv2.COLOR_BGR2RGB)
+        im = im[:1350, :, :]
+        xlen, ylen = im.shape[1], im.shape[0]
+        # fig = Figure((10, 6), dpi=200)  # make option
+        with plt.ioff():
+            fig, ax = plt.subplots((2, 1), figsize=(15, 15), dpi=200)
+        ax[0].imshow(im)
+        ax[1].imshow()
+        ax[0].set_title(fname)
+        # if xdata is not None and ydata is not None:
+        #     ax.plot([x for x in range(xlen)], [ydata] * xlen, color="firebrick" if not emphasize else "k")
+        self.disconnect_zoom = zoom_factory(ax)
+        self.pan_handler = panhandler(fig)
+        # fig.add_subplot(111).imshow(im)
         self.fig_canvas = FigureCanvasTkAgg(fig, master=self.canvas)
         self.fig_canvas.draw()
         self.toolbar = NavigationToolbar2Tk(self.fig_canvas, self.canvas, pack_toolbar=False)
         self.toolbar.update()
         self.fig_canvas.mpl_connect("key_press_event", key_press_handler)
-        self.fig_canvas.mpl_connect("button_press_event", on_click)
+        # self.fig_canvas.mpl_connect("button_press_event", self.fig_canvas_on_click)
         self.toolbar.pack()
-        self.fig_canvas.get_tk_widget().pack(expand=True)
+        self.fig_canvas.get_tk_widget().pack(fill=tk.BOTH, expand=True)
+        # self.fig_canvas.bind("<MouseWheel>", self.do_zoom)
+        # self.fig_canvas.mpl_connect("scroll_event", self.do_zoom)
+
+    def fig_canvas_on_click(self, event):
+        self._process_image(self.top_dir, self.fname, event.xdata, event.ydata)
 
     def process_next_image(self, top_dir, fname):
         self._process_image(top_dir, fname)
@@ -165,14 +179,8 @@ class MainApplication:
     def process_prev_image(self, top_dir, fname):
         self._process_image(top_dir, fname)
 
-    def switch_frame(self, frame_class):
-        # grid_config = frame_class.grid_config()
-        new_frame = frame_class(self.root)
-        if self._frame:
-            self._frame.destroy()
-        self._frame = new_frame
-        # self.root.
-        # self._frame.grid()
+    def process_reload_image(self, top_dir, fname):
+        self._process_image(top_dir, fname)
 
     def start(self):
         self.root.mainloop()
